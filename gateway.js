75f622f5b60c6a962a4df84fe0eb286fbbc8c4ba
Riaz115 <riaz90603@gmail.com>
2025-08-27 10:53:04 +0500

    solving cors errors

--- gateway.js
+++ gateway.js
@@ -15,20 +15,59 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:3000",
+//     "http://monkmaze-s3.s3-website.ap-south-1.amazonaws.com",
+//     "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:3000",
+//     "http://welkome.ca",
+//     "http://www.welkome.ca",
+//   ],
+//   methods: "GET,POST,PUT,DELETE,PATCH",
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+
+// Static file serving for uploaded images
+
+
+
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "http://monkmaze-s3.s3-website.ap-south-1.amazonaws.com",
-    "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:3000",
-    "http://welkome.ca",
-    "http://www.welkome.ca",
-  ],
-  methods: "GET,POST,PUT,DELETE,PATCH",
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl requests)
+    if (!origin) return callback(null, true);
+    
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "http://monkmaze-s3.s3-website.ap-south-1.amazonaws.com",
+      "https://monkmaze-s3.s3-website.ap-south-1.amazonaws.com", // HTTPS add karo
+      "http://ec2-13-245-5-146.af-south-1.compute.amazonaws.com:3000",
+      "http://welkome.ca",
+      "https://welkome.ca", // HTTPS add karo
+      "http://www.welkome.ca",
+      "https://www.welkome.ca" // HTTPS add karo
+    ];
+    
+    console.log('Request origin:', origin);
+    
+    if (allowedOrigins.includes(origin)) {
+      console.log('Allowed by CORS:', origin);
+      callback(null, true);
+    } else {
+      console.log('Blocked by CORS:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS", // OPTIONS add karo
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Headers specify karo
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
-// Static file serving for uploaded images
+
+
+
 app.use('/uploads', express.static('uploads'));
 
 // DB Connection
